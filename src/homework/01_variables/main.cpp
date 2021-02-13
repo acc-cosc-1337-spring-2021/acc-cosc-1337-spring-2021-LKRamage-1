e58fb5c580ce3d16734710e3b79666a54606bbce
LKRamage <lillian.kimmell@g.austincc.edu>
2021-02-12 20:58:58 -0600

    Homework2_LillianKimmell

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,5 +1,8 @@
 ﻿//write include statements
+#include "variables.h"
+#include<iostream>
 
+using std::cout; using std::cin;
 
 //write namespace using statement for cout
 
@@ -7,9 +10,32 @@
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
+
+
 int main()
 {
-	
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
 
+	cout<<"Enter meal amount: ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Enter tip rate percentage: ";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate)/100;
+
+	total = tip_amount + tax_amount +meal_amount;
+
+	cout<<"Meal Amount: "<<meal_amount<<"\n";
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+	cout<<"Tip Amount: "<<tip_amount<<"\n";
+	cout<<"Total: "<<total;
+	
 	return 0;
 }
