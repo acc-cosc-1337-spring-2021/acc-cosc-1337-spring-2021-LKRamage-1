cff5651e8c6b2f70bb0c2f557b894c2825b70a83
LKRamage <lillian.kimmell@g.austincc.edu>
2021-05-07 12:39:54 -0500

    Assignment 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -6,7 +6,33 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string &dna) 
+{
 
+    double content = 0.0;
+
+    int len = 0;
+
+    int i = 0;
+
+    while(dna[i] != '\0') {
+
+        if (dna[i] == 'G' || dna[i] == 'C') 
+        {
+
+            content += 1;
+
+        }
+
+        i += 1;
+
+        len += 1;
+
+    }
+
+    return content / len;
+
+}
 
 
 
@@ -14,7 +40,21 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string get_reverse_string(string dna) 
+{
 
+    string rev = "";
+
+    for (int i = dna.length()-1; i >= 0; i--) 
+    {
+
+        rev += dna[i];
+
+    }
+
+    return rev;
+
+}
 
 
 /*
@@ -27,4 +67,46 @@ b. iterate local string variable and
 c. return string
 
 */
+string get_dna_complement(string dna) 
+{
 
+    string reversed_string = get_reverse_string(dna);
+
+    string new_reversed;
+
+    for (int i = 0; i < reversed_string.length(); i++)
+    {
+
+        if (reversed_string[i] == 'A') 
+        {
+
+            new_reversed[i] = 'T';
+
+        }
+
+        else if(reversed_string[i] == 'T') 
+        {
+
+            new_reversed[i] = 'A';
+
+        }
+
+        else if(reversed_string[i] == 'C') 
+        {
+
+            new_reversed[i] = 'G';
+
+        }
+
+        else if(b[i] == 'G') 
+        {
+
+            new_reversed[i] = 'C';
+
+        }
+
+    }
+
+    return new_reversed;
+
+}
