e30c2e0a86e8fba13a4f01cd15ca96b53e18409b
LKRamage <lillian.kimmell@g.austincc.edu>
2021-05-06 21:14:01 -0500

    Assignment 4

--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,6 +1,11 @@
 //write includes statements
+#include <iostream>
+#include "loops.h"
+
 
 //write using statements for cin and cout
+using namespace std;
+
 
 
 /*
@@ -10,6 +15,29 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
-
-	return 0;
+do {
+      
+       cout << "Enter a number: ";
+       int number;
+       cin >> number;
+      
+       while(!(number >=1 && number <= 10)) {
+          
+           cout << "Invalid Number" << endl;
+          
+           cout << "Enter a number from 1 to 10: ";
+           cin >> number;
+       }
+      
+       cout << "Factorial of " << number << " is " << factorial(number) << endl;
+      
+       cout << "\nDo you want to continue (y/n): ";
+       char opt;
+       cin >> opt;
+       if(opt == 'n' || opt == 'N')
+           break;
+       cout << endl;
+   }while(true);
+  
+   return 0;
 }
