2b993f034a77d4d64f62782d647a9adc4b66299d
LKRamage <lillian.kimmell@g.austincc.edu>
2021-05-07 15:40:38 -0500

    Assignment 6

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -9,22 +9,22 @@ using std::string;
 int main() 
 
 {	
-	Game game;
+	Game TicTacToeGame;
 	string player;
 	int position;
 
-	cout<<"Let's Play Tic-Tac-Toe: Type either X or O to for your marker: ";
+	cout<<"Tic-Tac-Toe Game: Type X or O to choose your marker: ";
 	cin>>player;
 
-	game.start_game(player);
-	game.display_board();
+	TicTacToeGame.start_game(player);
+	TicTacToeGame.display_board();
 
 	do
 	{
-		cout<<"Player "<<game.get_player()<<" Choose from spots 1-9 to place your marker on the board! \nJust type '10' if you wish to close the board, ending the game: ";
+		cout<<"Player "<<TicTacToeGame.get_player()<<" Choose from spots 1-9 to place your marker on the board! \n Type '10' if you wish to close the board and end the game: ";
 		cin>>position;
-		game.mark_board(position);
-		game.display_board();
+		TicTacToeGame.mark_board(position);
+		TicTacToeGame.display_board();
 
 	}
 	while(position != 10);
