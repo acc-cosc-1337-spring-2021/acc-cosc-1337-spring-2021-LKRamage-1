19878c514b0579c367c4bfbb639771b5cde01b46
LKRamage <lillian.kimmell@g.austincc.edu>
2021-05-07 21:58:24 -0500

    Assignment 9

--- src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,5 +1,7 @@
 //cpp
 
+//void 
+
 #include "tic_tac_toe_manager.h"
 
 #include <iostream>
@@ -12,23 +14,28 @@ using std::cout;
 //using std::cin;
 
 //void TicTacToeManager::save_game(TicTacToe b)
-void TicTacToeManager::save_game(TicTacToe Game)
+void TicTacToeManager::save_game(unique_ptr<TicTacToe>& Game)
 {
-    games.push_back(Game);
-    update_winner_count(Game.get_winner());
+    //games.push_back(Game);
+    //update_winner_count(Game.get_winner());
+    update_winner_count(Game->get_winner());
+    games.push_back(std::move(Game));
 
 }
 
 //friend
-std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager)
+/* std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager)
 {
     for(int i = 0; i < manager.games.size(); i++)
     {
-        cout<<manager.games[i] <<"\n";
+        out<<manager.games[i] <<"\n";
     }
-
+    out <<"\n" <<"X Score: " <<x_win<<"\n";
+	out <<"O Score: " <<o_win<<"\n";
+	out <<"Ties: " <<ties<<"\n";
     return out;
-}
+}*/
+
 std::vector<TicTacToe> games;
 int x_win = 0;
 int o_win = 0;
@@ -57,4 +64,25 @@ void TicTacToeManager::update_winner_count(std::string winner)
         ties++;
     }
     
+}
+
+//friend
+std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager)
+{
+    /*for(int i = 0; i < manager.games.size(); i++)
+    {
+        out<<manager.games[i] <<"\n";
+    }*/
+    for(auto& Game : manager.games)
+    {
+        out << *Game;
+
+        out <<"\n";
+    }
+
+    out <<"\n" <<"X Score: " <<x_win<<"\n";
+	out <<"O Score: " <<o_win<<"\n";
+	out <<"Ties: " <<ties<<"\n";
+
+    return out;
 }
